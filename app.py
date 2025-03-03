e742b8a683e33a9fb2a7192768779b8e987293b2
clouduser2025 <hussainfarheen915@gmail.com>
2025-03-03 14:29:30 +0530

    FG

--- app.py
+++ app.py
@@ -792,4 +792,4 @@ async def get_market_data(username: str, token: str):
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 8000))  # Use Render's PORT or default to 8001 locally
-    uvicorn.run(app, host="0.0.0.0", port=port
+    uvicorn.run(app, host="0.0.0.0", port=port)
